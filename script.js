8486da17d76b75e71a4122420b403ac65dce43a6
Shreya Verma <72500761+shreya-v7@users.noreply.github.com>
2022-06-13 00:40:17 +0530

    style

--- script.js
+++ script.js
@@ -13,7 +13,26 @@ $(document).ready(function(){
     }); 
 });
 
-document.body.innerHTML += `<a href="#" id="scroll-button" style="display: none; z-index: 1000; position: fixed;"><span></span></a>
+document.body.innerHTML += `<a href="#" id="scroll-button" style="position:fixed;
+right:10px;
+bottom:10px;
+cursor:pointer;
+width:50px;
+height:50px;
+background-color:#000;
+text-indent:-9999px;
+display:none;
+-webkit-border-radius:60px;
+-moz-border-radius:60px;
+border-radius:60px; display: none; z-index: 1000; position: fixed;"><span style="position:absolute;
+top:50%;
+left:50%;
+margin-left:-8px;
+margin-top:-12px;
+height:0;
+width:0;
+border:8px solid transparent;
+border-bottom-color:#ffffff;"></span></a>
 <div id="mybutton" style="position: fixed;
   bottom: 75px;
   right: 10px;
