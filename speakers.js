b3db6e572e6a97d76252417ad4b09caf495146f8
Gurbir Dhaliwal <61262423+curious-gurbir@users.noreply.github.com>
2022-06-18 11:37:07 +0530

    Create speakers.js

---
+++ speakers.js
@@ -0,0 +1,12 @@
+var speakerData = [
+{
+  name:"Debby Soo",
+  title:"CEO of Opentable",
+  image:""
+},
+{
+  name:"Lesly Goh",
+  title:"Senior Technology Advisor and Former CTO of the World Bank",
+  image:""
+}
+];
